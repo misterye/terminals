ca000907486e1f8eff522a43627d7c4c8be33d7b
test <test@test.lan>
2018-02-11 13:43:51 +0800

    add pingtest scripts

---
+++ pingtest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+from time import sleep
+
+while True:
+    response = os.system('ping -c 30 10.8.0.1')
+    #print("The response is: %s" % response)
+
+    if response == 0:
+        print 'Connected!'
+    else:
+        print 'Disconnected!'
+        sudoPasswd = '33927569'
+        command = 'systemctl restart openvpn'
+        os.system('echo %s | sudo -S %s' % (sudoPasswd, command))
+        #os.system('sudo reboot')
+
+    sleep(3600)
