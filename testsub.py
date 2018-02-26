fbdc0a2b3c36b51ca2733e78a1295b211aa14441
misterye <zess1982@gmail.com>
2018-02-26 17:25:05 +0800

    store mqtt sub data to mysql db

--- testsub.py
+++ testsub.py
@@ -1,5 +1,6 @@
 import time
 import paho.mqtt.client as mqtt
+import MySQLdb
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -10,9 +11,24 @@ def on_connect(client, userdata, flags, rc):
         print("Connection failed.")
 
 def on_message(client, userdata, message):
-    print "Message received: " + message.payload
+    try:
+        clientstr = message.topic.split('/')[2]
+        db = MySQLdb.connect(host='localhost', user='root', passwd='33927569', db='test')
+        cur = db.cursor()
+        print "Storing client temperature data ..."
+        sql = ("""INSERT INTO temperature (data, client) VALUES (%s, %s)""", (message.payload, clientstr))
+        #sql = ("""INSERT INTO temperature (data) VALUES (%s)""", [message.payload])
+        cur.execute(*sql)
+        db.commit()
+        print "Done!"
+    except:
+        db.rollback()
+        print "Operation failed!"
+    cur.close()
+    print clientstr + ": " + message.payload
 
 broker = '111.47.20.166'
+topics = ['devices/raspi/gxgadzt/#', 'devices/raspi/xiqidongshu_test/#']
 Connected = False
 client = mqtt.Client("testsub")
 client.on_connect = on_connect
@@ -21,11 +37,12 @@ client.connect(broker, 1883, 60)
 client.loop_start()
 while Connected != True:
     time.sleep(0.1)
-client.subscribe("devices/raspi/gxgadzt/#")
+client.subscribe('devices/raspi/#')
 try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
     print "exiting..."
+    cur.close()
     client.disconnect()
     client.loop_stop()
