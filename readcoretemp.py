94665d7940416220a3c1bf19eb2f997c4883bf8a
misterye <zess1982@gmail.com>
2018-02-26 11:02:39 +0800

    add post readcoretemp by mqtt script

--- readcoretemp.py
+++ readcoretemp.py
@@ -1,8 +1,16 @@
 import os
-import requests
-import json
+import sys
+import time
 from time import sleep
+import paho.mqtt.client as mqtt
 
+MQTT_HOST = '111.47.20.166'
+INTERVAL = 2
+clientstr = 'xiqidongshu_test'
+client = mqtt.Client()
+next_reading = time.time()
+client.connect(MQTT_HOST, 1883, 60)
+client.loop_start()
 try:
     while True:
         os.system('sensors > /home/test/scripts/newcoretemp')
@@ -11,15 +19,17 @@ try:
         linestring = lines[6].split()
         tempstr = linestring[2][1:5]
         tempnum = float(tempstr)-10
-        newtempstr = str(tempnum)
-        #print newtempstr
-        data = {'pi_temp':newtempstr, 'pi_name':'xiqidongshu_test'}
-        #print data
-        try:
-            response = requests.post('http://139.224.114.83:8019/getTemp', json=data)
-        except requests.RequestException as e:
-            print(e.message)
+        #newtempstr = str(tempnum)
+        humidity = 30
+        client.publish('devices/raspi/clientstr', clientstr)
+        client.publish('devices/raspi/temperature', tempnum)
+        client.publish('devices/raspi/humidity', humidity)
+        next_reading += INTERVAL
+        sleep_time = next_reading - time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
         f.close()
-        sleep(10)
 except KeyboardInterrupt:
     pass
+client.loop_stop()
+client.disconnect()
