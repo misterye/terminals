bf41b2ee5122411434b6e7508ec85069c4970137
misterye <zess1982@gmail.com>
2018-02-27 14:02:09 +0800

    modify read_temp.py

--- read_temp.py
+++ read_temp.py
@@ -1,13 +1,29 @@
-import requests
+#!/usr/bin/python
+import sys
+import paho.mqtt.client as mqtt
 import os
+import time
+from time import sleep
 
-os.system('/opt/vc/bin/vcgencmd measure_temp > /home/pi/terminals/temp_log.log')
-with open('/home/pi/terminals/temp_log.log','r') as templog:
-    temp = templog.readline()
-newtemp = temp[5:9]
-# post temperature json to server
-data_from_pi = {'pi_temp':newtemp, 'pi_name':'hbwwdmz'}
+MQTT_HOST = '111.47.20.166'
+INTERVAL = 2
+client = mqtt.Client()
+next_reading = time.time()
+client.connect(MQTT_HOST, 1883, 60)
+client.loop_start()
 try:
-    response = requests.post('http://139.224.114.83:8019/getTemp', json=data_from_pi)
-except requests.RequestException as e:
-    print(e.message)
+    while True:
+        os.system('/opt/vc/bin/vcgencmd measure_temp > /home/pi/terminals/temp_log.log')
+        with open('/home/pi/terminals/temp_log.log','r') as templog:
+            temp = templog.readline()
+        tempstr = temp[5:9]
+        tempnum = float(tempstr)
+        client.publish('devices/raspi/hbwwdmz/temperature', tempnum)
+        next_reading += INTERVAL
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+client.loop_stop()
+client.disconnect()
