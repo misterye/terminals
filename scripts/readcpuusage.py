6219587365788e7f5d48f183462218ac4a2b218e
misterye <zess1982@gmail.com>
2018-02-27 03:10:20 +0000

    paho-mqtt cpu usage

--- scripts/readcpuusage.py
+++ scripts/readcpuusage.py
@@ -1,8 +1,15 @@
 import os
-import requests
-import json
+import sys
+import time
 from time import sleep
+import paho.mqtt.client as mqtt
 
+MQTT_HOST = '111.47.20.166'
+INTERVAL = 2
+client = mqtt.Client()
+next_reading = time.time()
+client.connect(MQTT_HOST, 1883, 60)
+client.loop_start()
 try:
     while True:
         f = open('cpu_usage_data', 'r')
@@ -10,14 +17,13 @@ try:
         linestring = lines[0].split()
         usagestr = linestring[1][0]
         usagenum = float(usagestr)
-        newusagestr = str(usagenum)+'%'
-        data = {'pi_temp':newusagestr, 'pi_name':'rnldmz'}
-        print data
-        try:
-            response = requests.post('http://139.224.114.83:8019/getTemp', json=data)
-        except requests.RequestException as e:
-            print(e.message)
+        client.publish('devices/raspi/rnldmz/temperature', usagenum)
+        next_reading += INTERVAL
+        sleep_time = next_reading - time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
         f.close()
-        sleep(6)
 except KeyboardInterrupt:
     pass
+client.loop_stop()
+client.disconnect()
